4e2932c20eb27e1704407b1dcdb73e9f20ca9d97
nhmayya <nhmayya@gmail.com>
2020-07-09 17:30:11 +0530

    database connection over

--- BACK-END/app.js
+++ BACK-END/app.js
@@ -1,10 +1,12 @@
 const express=require('express');
 const bodyParser=require('body-parser');
 const HttpError=require('./models/http-error');
+const mongoose=require('mongoose');
 const app=express();
 app.use(bodyParser.json());
 //app.use('/api/places',placeRoute);
 //app.use('/api/users',userRoute);
+//we have to import and use the routes folder
 app.use((req,res,next)=>{
     const error=new HttpError('Invalid URL',404);
     throw error;   
@@ -16,4 +18,13 @@ app.use((error,req,res,next)=>{
     res.status(error.code||500).json({message:error.message||"something went wrong in catch block..."});
 
 });
-app.listen(5000); 
+mongoose.connect('mongodb+srv://jhondeo:UWnO1ONk0r8YAWcV@cluster0.jt5tn.mongodb.net/hosting?retryWrites=true&w=majority')
+.then(()=>{
+    console.log('successfully connected to the database');
+    
+    app.listen(5000); 
+})
+.catch(err=>{
+    console.log(err);
+});
+
--- FRONT-END/pubg_tournment/src/Pages/signup.js
+++ FRONT-END/pubg_tournment/src/Pages/signup.js
@@ -9,3 +9,37 @@ const Signup=props=>{
 }
 
 export default Signup;
+
+//firebase phone authentication
+
+// import React, { Component } from 'react'
+// import firebase,{auth,provider} from './firebase.js';
+// export class App extends Component {
+//   handleClick=()=>{
+//     var recaptcha = new firebase.auth.RecaptchaVerifier('recaptcha');
+//     var number = '+917349585302';
+//     firebase.auth().signInWithPhoneNumber(number,recaptcha).then( function(e) {
+//       var code = prompt('Enter the otp', '');     
+//         if(code === null) return;      
+//         e.confirm(code).then(function (result) {
+//             console.log(result.user);
+//             document.querySelector('label').textContent +=   result.user.phoneNumber + "Number verified";  
+//         }).catch(function (error) {
+//             console.error( error);
+//         });
+//     })
+//     .catch(function (error) {
+//         console.error( error);
+//     });
+//   }
+//   render() {
+//     return (
+//       <div>
+//            <label></label>
+//         <div id="recaptcha"></div>
+//         <button onClick={this.handleClick}>Click</button>
+//       </div>
+//     )
+//   }
+// }
+// export default App
