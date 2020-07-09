cabe1bc80da9f21b9662eaeca3b7c3d542e1e7f8
nhmayya <nhmayya@gmail.com>
2020-07-09 11:53:04 +0530

    folder structure

---
+++ BACK-END/app.js
@@ -0,0 +1,19 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const HttpError=require('./models/http-error');
+//const app=express();
+app.use(bodyParser.json());
+//app.use('/api/places',placeRoute);
+//app.use('/api/users',userRoute);
+app.use((req,res,next)=>{
+    const error=new HttpError('Invalid URL',404);
+    throw error;   
+});
+app.use((error,req,res,next)=>{
+    if(res.headerSent){
+        return next(error);
+    }
+    res.status(error.code||500).json({message:error.message||"something went wrong in catch block..."});
+
+});
+app.listen(5000); 
