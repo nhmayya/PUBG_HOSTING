fcdb9cdeb9874076f513f4b2f10f1af4d619294b
datthaprasad <datthaprasad29@gmail.com>
2020-07-09 14:24:17 +0530

    Node modules for frontend

--- FRONT-END/pubg_tournment/src/App.js
+++ FRONT-END/pubg_tournment/src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import {BrowserRouter,Redirect,Switch,Route} from 'react-router-dom'
+
+import Home from './Pages/home'
+import Login from './Pages/login'
+import Signup from './Pages/signup'
 
 function App() {
   return (
-    <div>
-     hai
-    </div>
+    <React.Fragment>
+      <BrowserRouter>
+        <Switch>
+          <Route path='/login' exact>
+            <Login/>
+          </Route>
+          <Route path='/signup' exact>
+            <Signup/>
+          </Route>
+          <Route path='/' >
+            <Home/>
+          </Route>
+          <Redirect to='/'/>
+        </Switch>
+      </BrowserRouter>
+    </React.Fragment>
   );
 }
 
---
+++ FRONT-END/pubg_tournment/src/Pages/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home=props=>{
+    return(
+        <div>
+            home bro/ ANudeep work
+        </div>
+    )
+}
+
+export default Home;
